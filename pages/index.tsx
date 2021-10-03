61edf93493258c404784940a98e48747fac2f950
chandan-02 <anonymous012399@outlook.com>
2021-10-04 00:55:32 +0530

    essh

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex justify-center align-cente">
+    <div className="flex justify-center align-center">
       <h1>Note-san || Next & tailwind</h1>
     </div>
   )
