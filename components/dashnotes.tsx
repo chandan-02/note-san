473761ec47e455634a50c287a3d2ea881b94251d
chandan-02 <anonymous012399@outlook.com>
2021-10-16 17:51:57 +0530

    text overflow fix

--- components/dashnotes.tsx
+++ components/dashnotes.tsx
@@ -59,7 +59,7 @@ const DashNotes: FC<IDashNotes> = ({ note, db_user }) => {
     const styleMain = `bg-${note.color} my-2 md:m-5 w-full md:w-64 p-5 text-sm rounded filter hover:shadow-lg h-60 transition-all cursor-pointer flex flex-col justify-between`
     return (
         <div className={styleMain}>
-            <h1 className="font-poppins">{note.note.length > 140 ? note.note.substring(0, 200) + '.....' : note.note}</h1>
+            <h1 className={`font-poppins ${note.note.search(' ') > 27 && 'truncate'} ${note.note.search(' ') == -1 && 'truncate'}`}>{note.note.length > 140 ? note.note.substring(0, 200) + '.....' : note.note}</h1>
             <div className="flex items-center ">
                 <PencilAltIcon onClick={() => handleEdit()} className="h-5 w-5 text-black mr-4 hover:text-blue-700" />
                 {
