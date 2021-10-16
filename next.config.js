89e0e02cddb84e47a1b957aaea76183e9d6cd183
chandan-02 <anonymous012399@outlook.com>
2021-10-16 12:45:44 +0530

    API_BASE_URL fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   env:{
-    API_BASE_URL : 'http://localhost:3000'
+    API_BASE_URL : 'https://note-san.vercel.app/'
   },
   webpack: (config) => {
     config.experiments = { topLevelAwait: true };
