f679df388e5527e8f186027358a61873c4338130
Khushi-003 <orpadiakhushi@gmail.com>
2022-08-15 15:54:15 +0530

    good note

--- components/publicNotes.tsx
+++ components/publicNotes.tsx
@@ -1,54 +1,71 @@
-import { useRouter } from 'next/router';
-import { FC } from 'react';
+import { useRouter } from "next/router";
+import { FC } from "react";
 // import { UserCircleIcon } from '@heroicons/react/solid';
-import { IPublicNotes } from '../interface/notes';
+import { IPublicNotes } from "../interface/notes";
 
 /**
-   * PurgeCSS:
-   * bg-m_purple
-   * bg-m_light_orange
-   * bg-m_dark_orange
-   * bg-m_lime
-   * bg-m_blue
-   * ring-m_purple
-   * ring-m_light_orange
-   * ring-m_dark_orange
-   * ring-m_lime
-   * ring-m_blue
-**/
+ * PurgeCSS:
+ * bg-m_purple
+ * bg-m_light_orange
+ * bg-m_dark_orange
+ * bg-m_lime
+ * bg-m_blue
+ * ring-m_purple
+ * ring-m_light_orange
+ * ring-m_dark_orange
+ * ring-m_lime
+ * ring-m_blue
+ **/
 const PublicNotes: FC<IPublicNotes> = ({ note }) => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleView = () => {
-        if (note.id && note.shared_by) {
-            router.push({
-                pathname: `/public/note`,
-                query: {
-                    id: note.id,
-                    user: note.shared_by,
-                }
-            })
-        }
+  const handleView = () => {
+    if (note.id && note.shared_by) {
+      router.push({
+        pathname: `/public/note`,
+        query: {
+          id: note.id,
+          user: note.shared_by,
+        },
+      });
     }
+  };
 
-    return (
-        <div className="my-4 md:m-5 w-full md:w-64 text-sm " onClick={() => handleView()}>
-            <div className={`bg-${note.color} p-5 rounded filter hover:shadow-lg h-60 transition-all cursor-pointer flex flex-col justify-between`}>
-                <h1 className={`font-poppins ${note.note.search(' ') > 27 && 'truncate'} ${note.note.search(' ') == -1 && 'truncate'}`}>{note.note.length > 140 ? note.note.substring(0, 270) + '.....' : note.note}</h1>
-            </div>
-            <div className="flex items-center mt-3 cursor-default">
-                <div>
-                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                    <img src={note.user_img} className={`h-9 w-9 rounded-full ring-4 ring-${note.color}`} alt="user" />
-                </div>
-
-                <div className="ml-5">
-                    <h1 className="font-poppins text-gray-500 text-base">shared by</h1>
-                    <h1 className="font-poppins text-black text-sm">{note.shared_by}</h1>
-                </div>
-            </div>
+  return (
+    <div
+      className="my-4 md:m-5 w-full md:w-64 text-sm "
+      onClick={() => handleView()}
+    >
+      <div
+        className={`bg-${note.color} p-5 rounded filter hover:shadow-lg h-60 transition-all cursor-pointer flex flex-col justify-between`}
+      >
+        <h1
+          className={`font-poppins break-all ${
+            note.note.search(" ") > 27 && "truncate"
+          } ${note.note.search(" ") == -1 && "truncate"}`}
+        >
+          {note.note.length > 140
+            ? note.note.substring(0, 270) + "....."
+            : note.note}
+        </h1>
+      </div>
+      <div className="flex items-center mt-3 cursor-default">
+        <div>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={note.user_img}
+            className={`h-9 w-9 rounded-full ring-4 ring-${note.color}`}
+            alt="user"
+          />
         </div>
-    )
-}
+
+        <div className="ml-5">
+          <h1 className="font-poppins text-gray-500 text-base">shared by</h1>
+          <h1 className="font-poppins text-black text-sm">{note.shared_by}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default PublicNotes;
--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  env:{
-    API_BASE_URL : process.env.API_ENV == 'PRODUCTION' ? 'https://note-san.vercel.app' : 'http://localhost:3000'
+  env: {
+    API_BASE_URL: "http://localhost:3000",
+    GIT_ACCESS_TOKEN: "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",
+    HEROKU_POSTGRES_INSTANCE: "postgresql-crystalline-62055",
+    HEROKU_API_KEY: "68f6cbbd-f1a9-4bd1-8b17-0589b9714dd5",
+    Google_clientSecret: "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",
+    Google_clientId:
+      "950205729731-01h4qkbah0cav5unct0pm3k44j2cdcuc.apps.googleusercontent.com",
+    Git_clientId: "88775cbe68c434e9dea5",
+    NEXTAUTH_URL: "http://localhost:3000",
+    DB_URL:
+      "postgres://pjdytbpvljrdds:db1aa9916950d06a872fe01e73c6e48201e7ba7e69e8bcd98d0682be8f4a6ee2@ec2-44-198-29-193.compute-1.amazonaws.com:5432/d3mhq0t5ijt2v1",
   },
   webpack: (config) => {
     config.experiments = { topLevelAwait: true };
     return config;
   },
   reactStrictMode: true,
-}
+};
